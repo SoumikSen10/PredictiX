e31271cbad46dc07e75ed7665fd9c452baef5116
hallowshaw <rhitam.rit54@gmail.com>
2024-07-10 17:10:54 +0530

    Added Login and Predictor Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,89 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-import Navbar from "./components/Navbar.jsx";
-import Predictors from "./pages/PredictorsPage.jsx";
-import About from "./pages/AboutPage.jsx";
-import Login from "./pages/LoginPage.jsx";
-import Signup from "./pages/SignupPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
+import Navbar from "./components/Navbar";
+import PredictorsPage from "./pages/PredictorsPage";
+import AboutPage from "./pages/AboutPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import BreastPage from "./pages/BreastPage";
+import LungPage from "./pages/LungPage";
+import HeartPage from "./pages/HeartPage";
+import DiabetesPage from "./pages/DiabetesPage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/predictors" element={<Predictors />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          {/* Routes without Navbar */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
+          {/* Routes with Navbar */}
+          <Route
+            path="/"
+            element={
+              <div>
+                <Navbar />
+                <HomePage />
+              </div>
+            }
+          />
+          <Route
+            path="/predictors"
+            element={
+              <div>
+                <Navbar />
+                <PredictorsPage />
+              </div>
+            }
+          />
+          <Route
+            path="/predictors/breast"
+            element={
+              <div>
+                <Navbar />
+                <BreastPage />
+              </div>
+            }
+          />
+          <Route
+            path="/predictors/lung"
+            element={
+              <div>
+                <Navbar />
+                <LungPage />
+              </div>
+            }
+          />
+          <Route
+            path="/predictors/heart"
+            element={
+              <div>
+                <Navbar />
+                <HeartPage />
+              </div>
+            }
+          />
+          <Route
+            path="/predictors/diabetes"
+            element={
+              <div>
+                <Navbar />
+                <DiabetesPage />
+              </div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <div>
+                <Navbar />
+                <AboutPage />
+              </div>
+            }
+          />
         </Routes>
       </div>
     </Router>
--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Card({ image, title, description }) {
   return (
-    <div className="card">
+    <div>
       <img src={image} alt={title} className="card-image" />
-      <h3 className="card-title">{title}</h3>
+      <h2 className="card-title">{title}</h2>
       <p className="card-description">{description}</p>
     </div>
   );
 }
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Card;
