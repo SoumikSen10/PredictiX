16840c0f565ac90c2d93a65b018031a7ed1f3edc
Soumik Sen <mastersoumik2003@gmail.com>
2024-11-01 16:21:10 +0530

    Debugging:

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -9,12 +9,16 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
+      console.log(token)
+
     // console.log(token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    console.log(decodedToken)
 
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
